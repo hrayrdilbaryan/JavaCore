e3dba9b53b3776f66a01ba4a9baa351aebffb22d
hrayrdilbaryan <hrayr.dilbaryan@mail.ru>
2022-05-13 19:28:33 +0300

    figure2

---
+++ src/homework/Figure2.java
@@ -0,0 +1,16 @@
+package homework;
+
+public class Figure2 {
+    public static void main(String[] args) {
+        for (int i = 4; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+
+}
