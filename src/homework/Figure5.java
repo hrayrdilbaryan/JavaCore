2ac25311954774d23c1128a37d8026b43b560bbb
hrayrdilbaryan <hrayr.dilbaryan@mail.ru>
2022-05-16 14:15:32 +0300

    figure5

---
+++ src/homework/Figure5.java
@@ -0,0 +1,27 @@
+package homework;
+
+public class Figure5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
